b8a11000008a1f5e3537d82c42500da2c819f7ce
Bojan Mijuskovic <bojan.mijuskovic@montex-elektronika.com>
2016-02-23 09:04:50 +0100

    initial laravel application with a static page controller

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About</title>
+</head>
+<body>
+	<h1>About</h1>
+	<p>This is the application I'm building in the <a href="https://selftaughtcoders.com/from-idea-to-lunch"> From Idea To Lunch</a> course.</p>
+	<p>This is web application will allow people to both submit, and anwer, programming-related questions for various different programming languages.</p>
+</body>
+</html>
